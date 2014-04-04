0165b3e96d8eb15eca0f0389630c3abc87f5d6e3
Shane Forster <me@shaneforster.ca>
2014-04-04 19:59:17 -0400

    Initial commit

---
+++ src/ca/shaneforster/warfaresquare/WarMapFragment.java
@@ -0,0 +1,15 @@
+package ca.shaneforster.warfaresquare;
+
+import com.google.android.gms.maps.MapFragment;
+
+public class WarMapFragment extends MapFragment {
+
+	public WarMapFragment() {	
+	}
+	
+	@Override
+	public void onResume(){
+		
+	}
+
+}
