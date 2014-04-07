50f012002a7661db0ad1e2716746a3da6de75998
Shane Forster <me@shaneforster.ca>
2014-04-07 10:16:20 -0400

    Added Mayor and venue classes, finished attack dialog, added flags and coupon. Need network actions

--- src/ca/shaneforster/warfaresquare/MainActivity.java
+++ src/ca/shaneforster/warfaresquare/MainActivity.java
@@ -1,10 +1,17 @@
 package ca.shaneforster.warfaresquare;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
@@ -18,16 +25,16 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends Activity implements OnMarkerClickListener{
-	public static final String VENUE_NAME = "ca.shaneforster.warfaresquare.VENUE_NAME";
+	public static final String VENUE_ID = "ca.shaneforster.warfaresquare.VENUE_ID";
+	public static final String USER_NAME = "ca.shaneforster.warfaresquare.USER_NAME";
 	private MapFragment warMapFragment;
 	private GoogleMap warMap;
 	private DrawerLayout mDrawerLayout;
@@ -37,12 +44,31 @@ public class MainActivity extends Activity implements OnMarkerClickListener{
 	private CharSequence mDrawerTitle;
 	private CharSequence mTitle;
 	private String[] menuItems;
+	
+	private SharedPreferences prefs;
+	protected static Mayor user;
+	protected static HashMap<String, Venue> venues;
+	protected static LatLng userLocation;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		
+		prefs = getPreferences(MODE_PRIVATE);
+		
+		user = new Mayor(prefs.getString(USER_NAME, "default"));
+		
+		if ("default".equalsIgnoreCase(user.getUserName())){
+			//TODO launch login
+	
+		}
+		
+		//Demo code
+		user = new Mayor("Shane", 8, R.drawable.mk_flag_t4);
+		
+		venues = new HashMap<String, Venue>();
+		
 		warMapFragment = MapFragment.newInstance();
 		
 
@@ -85,6 +111,17 @@ public class MainActivity extends Activity implements OnMarkerClickListener{
 			}
 		};
 		mDrawerLayout.setDrawerListener(mDrawerToggle);
+		
+		Venue v;
+		v = new Venue("DMZ", new LatLng(43.656746, -79.380484), new Mayor("RichardLachman", 10, R.drawable.mk_flag_t1), 5);
+		venues.put(v.toString(), v);
+		
+		v = new Venue("Oakham House", new LatLng(43.657780, -79.379088), new Mayor("Geoff Lachapelle", 9, R.drawable.mk_flag_t3), 2);
+		venues.put(v.toString(), v);
+
+		v = new Venue("Podium", new LatLng(43.6583927, -79.380994), user, 4);
+		venues.put(v.toString(), v);
+		
 
 		if (savedInstanceState == null) {
 			selectItem(0);
@@ -129,25 +166,34 @@ public class MainActivity extends Activity implements OnMarkerClickListener{
 		
 		if (warMap == null)
 			Toast.makeText(this, "No Map!", Toast.LENGTH_LONG).show();
-		//TODO get nearby venues
+		
+		warMap.clear();
+		//TODO get nearby venues load into venues
 		
 		
-		Venue nearby[] = new Venue[25];
-		nearby[0] = new Venue("Name 1", new LatLng(43.656746, -79.380484), "Owner");
-		nearby[1] = new Venue("Name 2", new LatLng(43.657780, -79.379088), "Owner 2");
-		nearby[2] = new Venue("Name 3", new LatLng(43.6583927, -79.380994), "Owner 3");
-		
-		//TODO Draw nearby venues using
-
-		for (Venue v : nearby){
-			if (v != null)
-				warMap.addMarker(v.genMarkerOptions());
+		for (Map.Entry<String, Venue> ven : venues.entrySet()){
+			warMap.addMarker(ven.getValue().genMarkerOptions());
 		}
+
+		
 		warMap.setMyLocationEnabled(true);
 		warMap.setOnMarkerClickListener(this);
+
+	    Location location=((LocationManager)getSystemService(LOCATION_SERVICE)).getLastKnownLocation(((LocationManager)getSystemService(LOCATION_SERVICE)).getBestProvider(new Criteria(), true));
+	    userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+		
+	    warMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));
 		
 		
+	}
+	
+	
+	protected void onStop(){
+		super.onStop();
 		
+		SharedPreferences.Editor e = prefs.edit();
+		e.putString(USER_NAME, user.getUserName());
+		e.commit();
 	}
 
 	/* The click listner for ListView in the navigation drawer */
@@ -162,12 +208,13 @@ public class MainActivity extends Activity implements OnMarkerClickListener{
 
 	private void selectItem(int position) {
 		Fragment fragment = null;
-		Bundle args = new Bundle();
-
 		switch (position) {
 		case 0:
 			fragment = warMapFragment;
 			break;
+		case 1:
+			Intent i = new Intent(this, CouponActivity.class);
+			startActivity(i);
 		default:
 			Toast.makeText(this, "Not Implemented", Toast.LENGTH_LONG).show();
 			fragment = new Fragment();
@@ -206,19 +253,17 @@ public class MainActivity extends Activity implements OnMarkerClickListener{
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
-		// Pass any configuration change to the drawer toggls
+		// Pass any configuration change to the drawer toggles
 		mDrawerToggle.onConfigurationChanged(newConfig);
 	}
 	
 	public boolean onMarkerClick(Marker m){
-		//Filler
-		//Toast.makeText(this, m.getTitle(), Toast.LENGTH_LONG).show();
 		Intent intent = new Intent(this, AttackDialogActivity.class);
-		intent.putExtra(VENUE_NAME, m.getTitle());
+		intent.putExtra(VENUE_ID, m.getSnippet());
 		startActivity(intent);
-		//TODO Open Attack Menu
 		return true;
 	}
+	
 
 
 }
